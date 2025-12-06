70acc043e9a5200349548ccf3968999105b1c8bc
kunalms203 <kunalms203@gmail.com>
2025-12-06 14:33:36 +0000

    added new script

--- index.ts
+++ index.ts
@@ -1,17 +1,6 @@
 import express from "express"
 import "dotenv/config";
 import {prisma} from "./utils/db"
-import { console } from "inspector";
-
-const employee = await prisma.employees.create({
-    data: {
-      employee_code: "EMP001",
-      first_name: "Kunal",
-      last_name: "Shinde",
-      email: "kunal@example.com",
-      password_hash: "hashed_password_here"
-    }
-  });
 
 const users = await prisma.employees.findMany();
 console.log("all users",users);
