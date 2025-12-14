5e7a1f4ce33113c2c090a4ef7e5b2dfefda5477c
kunalms203 <kunalms203@gmail.com>
2025-12-14 09:32:14 +0000

    testing husky

--- index.ts
+++ index.ts
@@ -1,14 +1,14 @@
-import express from "express"
-import "dotenv/config";
-import mainRouter from "./routes/index"
+import express from 'express';
+import 'dotenv/config';
+import mainRouter from './routes/index';
 
 const app = express();
 const port = process.env.PORT;
 
 app.use(express.json());
 
-app.use("/api/v1",mainRouter);
+app.use('/api/v1', mainRouter);
 
-app.listen(port,()=>{
-    console.log(`App is listening on the http://localhost:${port}`);
-})
+app.listen(port, () => {
+  console.log(`App is listening on the http://localhost:${port}`);
+});
